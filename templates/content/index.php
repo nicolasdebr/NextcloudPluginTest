12bf9e7083124740dfff3a85b2ffb2808ff6822a
nicolasdebr <56118437+nicolasdebr@users.noreply.github.com>
2020-04-24 10:15:23 +0200

    Update index.php

--- templates/content/index.php
+++ templates/content/index.php
@@ -1,1 +1,21 @@
-<h1>Hello world</h1>
+<?php
+$fichier = "example.md";
+$total = 0;
+$ressource = fopen ($fichier, "r");
+$contenu = fread ($ressource, filesize ($fichier));
+fclose ($ressource);
+$tableau = explode("\r\n", $contenu);
+$longueurs = array();
+foreach($tableau as $ligne)
+{
+    $total = strlen($ligne)+$total;
+}
+$nb = count($tableau);
+for($i = 0; $i < $nb; $i++)
+{
+?>
+
+<?php
+}
+?>
+<p>Le document contiens <?= $total ?> caractères</p>
