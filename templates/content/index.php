1f3753482e643f867649503c39cc754a5578d9ad
nicolasdebr <56118437+nicolasdebr@users.noreply.github.com>
2020-04-24 15:40:23 +0200

    Update index.php

--- templates/content/index.php
+++ templates/content/index.php
@@ -1,23 +1,39 @@
 <?php
-echo $file;
-$fichier = $file;
-$total = 0;
-$ressource = fopen ($fichier, "r");
-$contenu = fread ($ressource, filesize ($fichier));
-fclose ($ressource);
-$tableau = explode("\r\n", $contenu);
-$longueurs = array();
-foreach($tableau as $ligne)
-{
-    $total = strlen($ligne)+$total;
-}
-$nb = count($tableau);
-for($i = 0; $i < $nb; $i++)
-{
-?>
-
 <?php
-}
-?>
+namespace OCA\MyApp\Storage;
 
+class AuthorStorage {
+
+    private $storage;
+
+    public function __construct($storage){
+        $this->storage = $storage;
+    }
+
+    public function getContent($id) {
+        // check if file exists and read from it if possible
+        try {
+            $file = $this->storage->getById($id);
+            if($file instanceof \OCP\Files\File) {
+                $fichier = $file;
+                $total = 0;
+                $ressource = fopen ($fichier, "r");
+                $contenu = fread ($ressource, filesize ($fichier));
+                fclose ($ressource);
+                $tableau = explode("\r\n", $contenu);
+                $longueurs = array();
+                foreach($tableau as $ligne)
+                {
+                    $total = strlen($ligne)+$total;
+                }
+                $nb = count($tableau);
+                return $total;
+            } else {
+                throw new StorageException('Can not read from folder');
+            }
+        } catch(\OCP\Files\NotFoundException $e) {
+            throw new StorageException('File does not exist');
+        }
+    }
+}
 <p>Le document contiens <?= $total ?> caractères</p>
