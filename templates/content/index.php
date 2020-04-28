7d46e78e3b7f1d555ff0a03e9515691edc52bd58
nicolasdebr <56118437+nicolasdebr@users.noreply.github.com>
2020-04-28 13:39:03 +0200

    Update index.php

--- templates/content/index.php
+++ templates/content/index.php
@@ -1,22 +1,4 @@
-$fichier = $TheFile;
-$total = 0;
-$ressource = fopen ($fichier, "r");
-$contenu = fread ($ressource, filesize ($fichier));
-fclose ($ressource);
-$tableau = explode("\r\n", $contenu);
-$longueurs = array();
-foreach($tableau as $ligne)
-{
-    $total = strlen($ligne)+$total;
-}
-$nb = count($tableau);
-for($i = 0; $i < $nb; $i++)
-{
-?>
 
-<?php
-}
-?>
 <p>Le document contiens <?= $total ?> caractères</p>
     
 <div id="search">
