abcaf317d6f3cc292a52bd661f897b88e677c74a
nicolasdebr <56118437+nicolasdebr@users.noreply.github.com>
2020-04-28 13:39:54 +0200

    Update PageController.php

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -25,8 +25,27 @@ class PageController extends Controller {
 	 * @NoCSRFRequired
 	 */
 	public function index() {
-		$TheFile = $this->request->getUploadedFile($file);
-		return new TemplateResponse('grafanadelamor', 'index',$TheFile);  // templates/index.php
+		$TheFile = $this->request->getUploadedFile('questions.txt');
+		$fichier = $TheFile;
+$total = 0;
+$ressource = fopen ($fichier, "r");
+$contenu = fread ($ressource, filesize ($fichier));
+fclose ($ressource);
+$tableau = explode("\r\n", $contenu);
+$longueurs = array();
+foreach($tableau as $ligne)
+{
+    $total = strlen($ligne)+$total;
+}
+$nb = count($tableau);
+for($i = 0; $i < $nb; $i++)
+{
+?>
+
+<?php
+}
+?>
+		return new TemplateResponse('grafanadelamor', 'index',$total);  // templates/index.php
 	}
 	public function GetFile($file){
 		
