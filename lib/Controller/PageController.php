0b8ba8ad5cdacb2297dab5217485ed7d220ff049
nicolasdebr <56118437+nicolasdebr@users.noreply.github.com>
2020-04-28 15:03:10 +0200

    Update PageController.php

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -1,6 +1,6 @@
 <?php
 namespace OCA\GrafanaDelAmor\Controller;
-include 'Storage.php';
+use OCA\GrafanaDelAmor\Storage;
 use OCP\IRequest;
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\AppFramework\Http\DataResponse;
