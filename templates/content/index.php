23aa981d0f0e9acc4c58af52ed3a9816f80298bc
nicolasdebr <56118437+nicolasdebr@users.noreply.github.com>
2020-04-28 13:34:29 +0200

    Update index.php

--- templates/content/index.php
+++ templates/content/index.php
@@ -3,6 +3,5 @@
     <form id="search-form">
         <input type="text" placeholder="" id="search-term" />
         <input type="submit" id="search-submit" value="Count" >
-        <button id="route-submit" class=""></button>
     </form>
 </div>
