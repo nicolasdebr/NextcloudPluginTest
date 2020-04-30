c17177d993496f96ec5948cb70af72e406d16fde
nicolasdebr <56118437+nicolasdebr@users.noreply.github.com>
2020-04-30 13:56:31 +0200

    Update index.php

--- templates/content/index.php
+++ templates/content/index.php
@@ -4,6 +4,13 @@
 <div id="search">
     <form id="search-form">
         <input type="text" placeholder="" id="search-term" />
-        <input type="submit" id="search-submit" value="Count" />
+        <input type="submit" id="button1" value="Count" />
     </form>
 </div>
+<script>
+var element = document.getElementById("button1");
+element.addEventListener("click", function()
+{
+ alert("OK");
+});
+</script>
