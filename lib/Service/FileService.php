1477f598c582e3052b4d02d044577fa2ae88041c
nicolasdebr <56118437+nicolasdebr@users.noreply.github.com>
2020-05-04 12:22:08 +0200

    Update FileService.php

--- lib/Service/FileService.php
+++ lib/Service/FileService.php
@@ -1,9 +1,18 @@
 <?php
 namespace OCA\grafanadelamor\Service;
 
+use OCA\grafanadelamor\Db\File;
+
 class FileService {
   
   public function create (string $tittle, string $content, string $userId){
-    return "Service : " . $tittle . $content . $userId;
+    //return "Service : " . $tittle . $content . $userId;
+  $file = new file();
+  $file->setTitle($tittle);
+  $file->setContent($content);
+  $file->setUserId($userId);  
+  
+    return $file;
+  
   }
 }
