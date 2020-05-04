6ad79c257425315f3cd970d87b225acd2f065b79
nicolasdebr <56118437+nicolasdebr@users.noreply.github.com>
2020-05-04 13:57:24 +0200

    Update FileService.php

--- lib/Service/FileService.php
+++ lib/Service/FileService.php
@@ -12,10 +12,10 @@ class FileService {
   $this->mapper = $mapper;
   }
   
-  public function create (string $tittle, string $content, string $userId){
+  public function create (string $title, string $content, string $userId){
     //return "Service : " . $tittle . $content . $userId;
   $file = new File();
-  $file->setTitle($tittle);
+  $file->setTitle($title);
   $file->setContent($content);
   $file->setUserId($userId);  
   
