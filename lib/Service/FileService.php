347d71b04cda5b6cc147efe16f2bf69022d2c88b
nicolasdebr <56118437+nicolasdebr@users.noreply.github.com>
2020-05-04 13:46:27 +0200

    Update FileService.php

--- lib/Service/FileService.php
+++ lib/Service/FileService.php
@@ -13,7 +13,7 @@ class FileService {
   
   public function create (string $tittle, string $content, string $userId){
     //return "Service : " . $tittle . $content . $userId;
-  $file = new file();
+  $file = new File();
   $file->setTitle($tittle);
   $file->setContent($content);
   $file->setUserId($userId);  
